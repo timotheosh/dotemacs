128e982a42efc67a6c4e75bd12256aa8813e9f22
Tim Hawes <tim@selfdidactic.com>
2015-11-29 22:32:40 -0500

    Commented out unworking code in lisp-init.el, that appears to have been keeping code completion from working in a lisp file buffer.

--- lisp/lisp-init.el
+++ lisp/lisp-init.el
@@ -8,12 +8,12 @@
     (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
     (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 
-    (defun my:slime-eval-last-expression()
-        (unless (slime-connected-p)
-            (save-excursion (slime)))
-        (slime-eval-last-expression))
-    (add-hook 'lisp-mode-hook (lambda ()
-        (local-set-key (kbd "C-c C-c") #'my:slime-eval-last-expression)))
+    ;(defun my:slime-eval-last-expression()
+    ;    (unless (slime-connected-p)
+    ;        (save-excursion (slime)))
+    ;    (slime-eval-last-expression))
+    ;(add-hook 'lisp-mode-hook (lambda ()
+    ;    (local-set-key (kbd "C-c C-c") #'my:slime-eval-last-expression)))
 
     (require 'ac-slime)
     (add-hook 'slime-mode-hook 'set-up-slime-ac)
