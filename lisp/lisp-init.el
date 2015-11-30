b99836afac1619933487b89235fd81880693f527
Tim Hawes <tim@selfdidactic.com>
2015-11-29 21:38:33 -0500

    Fixed issues with SLIME. Added animation.

--- lisp/lisp-init.el
+++ lisp/lisp-init.el
@@ -2,15 +2,23 @@
 (use-package slime
     :init
     (add-to-list 'ac-modes 'lisp-mode)
-    (setq slime-contribs '(slime-fancy))
+    (setq slime-contribs '(slime-fancy slime-banner))
     (setq inferior-lisp-program (expand-file-name "/home/thawes/programs/bin/sbcl"))
+    (setq slime-startup-animation t)
     (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
     (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+
+    (defun my:slime-eval-last-expression()
+        (unless (slime-connected-p)
+            (save-excursion (slime)))
+        (slime-eval-last-expression))
+    (add-hook 'lisp-mode-hook (lambda ()
+        (local-set-key (kbd "C-c C-c") #'my:slime-eval-last-expression)))
 
     (require 'ac-slime)
     (add-hook 'slime-mode-hook 'set-up-slime-ac)
     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
     (eval-after-load "auto-complete"
       '(add-to-list 'ac-modes 'slime-repl-mode))
-    (slime-setup)
-    (slime))
+    (slime-setup))
+
