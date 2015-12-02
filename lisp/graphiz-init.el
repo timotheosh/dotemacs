5f8814be11baf2ac238f59c9da4e7634c067c538
Tim Hawes <tim@selfdidactic.com>
2015-12-01 19:19:16 -0500

    Fixed bug in the graphiz-init file

--- lisp/graphiz-init.el
+++ lisp/graphiz-init.el
@@ -2,4 +2,4 @@
 (use-package graphiz-dot-mode-autoloads
   :init
   (add-hook 'graphiz-dot-mode-hook
-            (local-set-key (kbd "C-c C-p") 'graphiz-dot-preview))
+            (local-set-key (kbd "C-c C-p") 'graphiz-dot-preview)))
