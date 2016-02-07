c98bb9cff1d5c5c2bdbf53a5ca519b6576b51cb9
Tim Hawes <tim@selfdidactic.com>
2016-02-07 06:54:34 -0500

    Added rust-mode

---
+++ layers/rust/rust-init.el
@@ -0,0 +1,46 @@
+;;; rust-init.el --- Initialize rust-mode
+
+;; Copyright (C) 2016 Free Software Foundation, Inc.
+;;
+;; Author: Tim Hawes <tim@easyfreeunix.com>
+;; Maintainer: Tim Hawes <tim@easyfreeunix.com>
+;; Created: 06 Feb 2016
+;; Version: 0.01
+;; Keywords
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;;
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+;;   (require 'rust-init)
+
+;;; Code:
+
+(eval-when-compile
+  (require 'cl))
+
+(use-package rust-mode
+  :mode (("\\.rs\\'" . rust-mode)
+         ("\\.toml\\'" . rust-mode))
+  :init
+  (autoload 'rust-mode "rust-mode" nil t)
+  (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+(provide 'rust-init)
+;;; rust-init.el ends here
