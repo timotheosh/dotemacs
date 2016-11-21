b6569131596cbc1a9a56f4fe19b284616873a620
Tim Hawes <tim.hawes@inin.com>
2016-11-21 10:28:04 -0500

    Add a key-binding for creating a text buffer

--- layers/customizations/custom-keys.el
+++ layers/customizations/custom-keys.el
@@ -10,15 +10,22 @@
       (ecb-activate)
       (ecb-toggle-ecb-windows))))
 
+(defun create-text-scratch-buffer nil
+  "create a text scratch buffer"
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*text-scratch*"))
+  (text-mode))
+
 (global-set-key [f1] 'dired)
-;(global-set-key [f2] 'mu4e)
+;;(global-set-key [f2] 'mu4e)
 (global-set-key [f3] 'shell)
 (global-set-key [f4] 'find-file)
-; for helm
-;(global-set-key [f4] #'helm-find-files)
-;(global-set-key [f5] 'compile)
-;(global-set-key [f6] 'kill-this-buffer)
-;(global-set-key [f7] 'make-frame)
+;; for helm
+;;(global-set-key [f4] #'helm-find-files)
+;;(global-set-key [f5] 'compile)
+(global-set-key [f5] 'create-text-scratch-buffer)
+;;(global-set-key [f6] 'kill-this-buffer)
+;;(global-set-key [f7] 'make-frame)
 (global-set-key [f9] 'menu-bar-mode)
 (global-set-key [f10] 'my/activate-ecb)
 
