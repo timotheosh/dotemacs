c0a2fd842a9bf8091912ddb6d8f22425a95dcf5b
Tim Hawes <tim@easyfreeunix.com>
2017-02-06 22:12:10 -0500

    Added erc, use shell path (probably remove at a later date), and bookmarks file for eww.

---
+++ layers/IRC/erc-init.el
@@ -0,0 +1,23 @@
+(use-package erc
+  :init
+  (setq erc-autojoin-channels-alist
+        '(("freenode.net"
+           "##C"
+           "##C++-general"
+           "#clang"
+           "#clnoobs"
+           "#clojure"
+           "#clojure-beginners"
+           "#clojure-emacs"
+           "#clojurescript"
+           "#cmake"
+           "#cplusplus.com"
+           "#emacs"
+           "#emacs-beginners"
+           "#lisp"
+           "#llvm"
+           "#racket"
+           "#scheme"
+           "#sword"
+           "#ubuntu"))))
+(provide 'erc-init)
