ff91de4d2a0f3885cb8da8170cb7f99c0b44698d
Tim Hawes <tim@selfdidactic.com>
2017-03-12 18:08:04 -0400

    Improve hy-mode some.

--- layers/python/hy-init.el
+++ layers/python/hy-init.el
@@ -1,9 +1,14 @@
 ;; Hy mode
+;;
+;; You can get an hy repl by typing in M-x inferior-lisp while in a hy
+;; buffer. Needs some work to send buffer to the repl, and only open a
+;; new repl if one does not already exist for the active hy buffer.
 (use-package hy-mode
   :ensure t
   :mode ("\\.hy\\'" . hy-mode)
   :config
   (require 'programming-init)
+  (setq hy-mode-inferior-lisp-command "hy")
   :init
   ;; Add smartparens-strict-mode
   (dolist (func '(my-programming-hooks
