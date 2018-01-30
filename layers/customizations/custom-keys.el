11f53cbee9a6205625cbf3e5d282a40dd9686c1c
Tim Hawes <tim.hawes@genesys.com>
2018-01-30 11:52:13 -0500

    Added projectile-speedbar-mode. Added sensible keybindings for it (or attempted to)

--- layers/customizations/custom-keys.el
+++ layers/customizations/custom-keys.el
@@ -12,5 +12,6 @@
 (global-set-key [f4] 'find-file)
 (global-set-key [f5] 'create-text-scratch-buffer)
 (global-set-key [f9] 'menu-bar-mode)
+(global-unset-key [M-f10])  ; Reserved for projectile-speedbar
 
 (provide 'custom-keys)
--- layers/programming/sr-speedbar-init.el
+++ layers/programming/sr-speedbar-init.el
@@ -2,5 +2,8 @@
   :ensure sr-speedbar
   :bind (([f10] . sr-speedbar-toggle))
   :config
+  (use-package projectile-speedbar
+    :ensure t
+    :bind (([M-f10] . projectile-speedbar-open-current-buffer-in-tree)))
   (setq sr-speedbar-right-side nil))
 (provide 'sr-speedbar-init)
