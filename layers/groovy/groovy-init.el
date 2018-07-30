cdec08f324d9758229ac2769a16a1d56339399ac
Tim Hawes <trhawes@gmail.com>
2018-07-30 09:10:57 -0400

    Added *.jenkinsfile suffix to groovy-mode

--- layers/groovy/groovy-init.el
+++ layers/groovy/groovy-init.el
@@ -1,5 +1,6 @@
 (use-package groovy-mode
   :mode (("\\.groovy\\'" . groovy-mode)
-         ("Jenkinsfile" . groovy-mode))
+         ("Jenkinsfile" . groovy-mode)
+         ("\\.jenkinsfile\\'" . groovy-mode))
   :ensure t)
 (provide 'groovy-init)
