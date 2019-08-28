6d8683737f392d5813e069f2eabef4b9ef990893
Tim Hawes <trhawes@gmail.com>
2019-08-27 22:35:17 -0400

    Customizations that enhance lsp-ui

--- layers/customizations/custom.el
+++ layers/customizations/custom.el
@@ -13,10 +13,11 @@
    (quote
     (helm-lsp dap-mode lsp-treemacs company-lsp lsp-ui avy esh-help all-the-icons-ivy multi-eshell all-the-icons-dired-mode all-the-icons web-server eshell-prompt-extras restclient flycheck-pos-tip aws-snippets company-quickhelp xref-js2 which-key wgrep-ag web-mode virtualenvwrapper use-package tree-mode smex smartparens smart-dash slime-company rainbow-delimiters racket-mode quack python-pytest python-mode python-docstring powerline pipenv pip-requirements php-mode persp-projectile paradox ox-gfm origami org-projectile org-jira org-bullets neotree multi-term markdown-mode magit lisp-extra-font-lock keychain-environment json-mode js2-refactor jedi jdee javadoc-lookup java-imports irony-eldoc iedit hy-mode ht helm haskell-mode groovy-mode general geiser geben flymake-racket flycheck-rust flycheck-pycheckers flycheck-irony flycheck-cython flycheck-clojure flycheck-clangcheck flycheck-clang-tidy flycheck-clang-analyzer exec-path-from-shell elpy dr-racket-like-unicode dockerfile-mode docker-compose-mode darktooth-theme cython-mode counsel-projectile company-tern company-irony company-c-headers company-ansible column-enforce-mode cmake-ide clojure-mode-extra-font-locking clj-refactor chicken-scheme cargo bui blacken basic-mode auto-virtualenv ansible aggressive-indent ac-slime 4clojure)))
  '(paradox-github-token t)
- '(safe-local-variable-values (quote ((eval ansible 1)))))
+ '(safe-local-variable-values (quote ((eval ansible 1))))
+ '(lsp-ui-sideline-current-symbol ((t (:foreground "gray" :box (:line-width -1 :color "cyan") :weight ultra-bold :height 0.99)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-code-face ((t (:inherit consolas)))))
